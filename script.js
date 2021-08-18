091ed555b02cea06a949c13986e20cac0b014bd9
Jona <mr-jona@hotmail.com>
2021-08-18 18:26:01 +0200

    finished the resolution input to be functional

--- script.js
+++ script.js
@@ -51,3 +51,62 @@ clearBtn.addEventListener('click', clearGrid);
 
 
 
+
+
+
+
+
+function newInputtedGrid() {
+    drawingArea.innerHTML = ""; //deletes the previous Grid div
+
+    let gridResInput = document.querySelector('.gridResInput').value;
+
+    console.log(gridResInput);
+
+
+    function createNewGrid() {
+
+        let grid = document.createElement('div'); 
+        grid.classList.add('grid')
+        grid.style.cssText = `grid-template-columns: repeat(${gridResInput}, 1fr);
+        grid-template-rows: repeat(${gridResInput}, 1fr)`;    
+
+
+
+        for (let i = 0; i < gridResInput*gridResInput; i++) {   
+            let pixel = document.createElement('div');
+    
+            pixel.classList.add('pixel');
+    
+            grid.appendChild(pixel);
+        }
+       
+        drawingArea.appendChild(grid);
+    
+    };
+    
+    createNewGrid();
+    
+  // HAD TO REDECLARE THESE VARIABLE AND FUNCTIONS, NOT SURE WHY. (it might not recognize the .pixel class anymore for these new divs)
+  
+    let allPixels = document.querySelectorAll('.pixel');
+    allPixels.forEach(pixel => pixel.addEventListener("mouseenter", draw));
+
+
+
+    function clearGrid() {
+        allPixels.forEach(pixel => pixel.style.cssText = "background-color: ");
+    }
+    
+
+    let clearBtn = document.querySelector('.clearButton');
+    clearBtn.addEventListener('click', clearGrid);
+    
+
+}
+
+
+
+
+let gridResBtn = document.querySelector('.gridResBtn');
+gridResBtn.addEventListener('click', newInputtedGrid);
