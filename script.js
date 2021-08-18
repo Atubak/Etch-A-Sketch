e7c9d383795ef96f28ec2f20cf83505dd3d45d7c
Jona <mr-jona@hotmail.com>
2021-08-18 15:11:19 +0200

    improved the grid, made the basic sketching work

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 let drawingArea = document.querySelector('.drawingArea');
 
-function createPixels() {
-    for (let i = 0; i < 256; i++) {    
+
+
+function createOneSixGrid() {
+
+    let oneSix = document.createElement('div'); 
+    oneSix.classList.add('oneSix')
+
+    for (let i = 0; i < 16*16; i++) {   
         let pixel = document.createElement('div');
+
         pixel.classList.add('pixel');
-        pixel.textContent = 'a';
 
-
-        drawingArea.appendChild(pixel);
+        oneSix.appendChild(pixel);
     }
+   
+    drawingArea.appendChild(oneSix);
+
 };
 
-createPixels();
+createOneSixGrid();
+
+
+function draw(e) {
+    e.target.style.cssText = "background-color: black";
+}
+
+
+let allPixels = document.querySelectorAll('.pixel');
+allPixels.forEach(pixel => pixel.addEventListener("mouseenter", draw));
+
